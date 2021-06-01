b6ca0b724a2e69431ad4310b0b897c7049100dbf
Durgaprasath01415 <sdp8754262827@gmail.com>
2021-06-01 10:11:00 +0530

    MODIFIED: Removed try and catch for all except checkEmailValid

--- src/test/java/UserRegistationTest.java
+++ src/test/java/UserRegistationTest.java
@@ -38,102 +38,74 @@ public class UserRegistationTest {
     @Test
     public void givenFirstName_whenProper_shouldReturnTrue() {
         UserRegistation userRegistation = new UserRegistation();
-        try{
-            boolean result = userRegistation.validateFirstName("Durga");
-            Assert.assertEquals(true, result);
-        }   catch (UserRegistrationInvalidException ex){
-            Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
-        }
+        boolean result = userRegistation.validateFirstName("Durga");
+        Assert.assertEquals(true, result);catch (UserRegistrationInvalidException ex)
+        Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
+
     }
     @Test
     public void givenFirstName_whenNotProper_shouldReturnfalse() {
         UserRegistation userRegistation = new UserRegistation();
-        try{
-            boolean result = userRegistation.validateFirstName("durga");
-            Assert.assertEquals(false, result);
-        }   catch (UserRegistrationInvalidException ex){
-            Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
-        }
+        boolean result = userRegistation.validateFirstName("durga");
+        Assert.assertEquals(false, result);
+        Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
     }
     @Test
     public void givenFirstName_whenShort_shouldReturnfalse() {
         UserRegistation userRegistation = new UserRegistation();
-        try{
-            boolean result = userRegistation.validateFirstName("Du");
-            Assert.assertEquals(false, result);
-        }   catch (UserRegistrationInvalidException ex){
-            Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
-        }
+        boolean result = userRegistation.validateFirstName("Du");
+        Assert.assertEquals(false, result);
+        Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
+
     }
     @Test
     public void givenLastName_whenProper_shouldReturnTrue() {
         UserRegistation userRegistation = new UserRegistation();
-        try{
-            boolean result = userRegistation.validateLastName("Prasath");
-            Assert.assertEquals(true, result);
-        }   catch (UserRegistrationInvalidException ex){
-            Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
-        }
+        boolean result = userRegistation.validateLastName("Prasath");
+        Assert.assertEquals(true, result);
+        Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
     }
     @Test
     public void givenLastName_whenNotProper_shouldReturnfalse() {
         UserRegistation userRegistation = new UserRegistation();
-        try{
-            boolean result = userRegistation.validateLastName("prasath");
-            Assert.assertEquals(false, result);
-        }   catch (UserRegistrationInvalidException ex){
-            Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
-        }
+        boolean result = userRegistation.validateLastName("prasath");
+        Assert.assertEquals(false, result);
+        Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
     }
     @Test
     public void givenLastName_whenShort_shouldReturnfalse() {
         UserRegistation userRegistation = new UserRegistation();
-        try{
-            boolean result = userRegistation.validateLastName("pr");
-            Assert.assertEquals(false, result);
-        }   catch (UserRegistrationInvalidException ex){
-            Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
-        }
+        boolean result = userRegistation.validateLastName("pr");
+        Assert.assertEquals(false, result);
+        Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
     }
     @Test
     public void givenPhoneNumber_whenProper_shouldReturnTrue() {
         UserRegistation userRegistation = new UserRegistation();
-        try {
-            boolean result = userRegistation.validatePhoneNumber("91 9919819801");
-            Assert.assertEquals(true, result);
-        }   catch (UserRegistrationInvalidException ex){
-            Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
-        }
+        boolean result = userRegistation.validatePhoneNumber("91 9919819801");
+        Assert.assertEquals(true, result);
+        Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
     }
     @Test
     public void givenPhoneNumber_whenNotProper_shouldReturnfalse() {
         UserRegistation userRegistation = new UserRegistation();
-        try{
-            boolean result = userRegistation.validatePhoneNumber("91 99198 19801");
-            Assert.assertEquals(false, result);
-        }   catch (UserRegistrationInvalidException ex){
-            Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
-        }
+        boolean result = userRegistation.validatePhoneNumber("91 99198 19801");
+        Assert.assertEquals(false, result);
+        Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
     }
     @Test
     public void givenPassWord_whenProper_shouldReturnTrue() {
         UserRegistation userRegistation = new UserRegistation();
-        try{
-            boolean result = userRegistation.validatePassWord("aA12345@!");
-            Assert.assertEquals(true, result);
-        }   catch (UserRegistrationInvalidException ex){
-            Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
-        }
+        boolean result = userRegistation.validatePassWord("aA12345@!");
+        Assert.assertEquals(true, result);
+        Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
     }
     @Test
     public void givenPassWord_whenNotProper_shouldReturnfalse() {
         UserRegistation userRegistation = new UserRegistation();
-        try {
-            boolean result = userRegistation.validatePassWord("AAA12345@");
-            Assert.assertEquals(false, result);
-        }   catch (UserRegistrationInvalidException ex){
-            Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
-        }
+        boolean result = userRegistation.validatePassWord("AAA12345@");
+        Assert.assertEquals(false, result);
+        Assert.assertEquals(UserRegistrationInvalidException.class, ex.getClass());
     }
     @Test
     public void testMoodAnalysis_whenSad_shouldReturnSad() {
